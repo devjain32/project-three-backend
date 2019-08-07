f243f3e552a5b4913aa44f021809fdc8946ecd6b
Megan Wu <meganwu1994@gmail.com>
2019-08-07 10:26:52 -0500

    textarea for entering notes added

--- client/src/components/GardenResult/index.js
+++ client/src/components/GardenResult/index.js
@@ -4,8 +4,23 @@ import rose from "./rose.jpg";
 
 function GardenResult(){
     return (
-        <div className="garden">
-            <img id="rose" src={rose} alt="garden-section"/>
+        <div className="parent">
+            <div className="garden">
+                <img id="rose" src={rose} alt="garden-section"/>
+            </div>
+
+            <div className="notes">
+                <h3>Tips for taking care of this plant :</h3>
+                <ul>
+                    <li>1. Water it twice a day</li>
+                    <li>2. Need sunshine for at least 2 hrs per day</li>
+                    <li>3. Keep it in a dry area</li>
+                </ul>
+
+                <textarea>Enter your note...</textarea>
+                <button>Add a note</button>
+
+            </div>
         </div>
     );
 }
