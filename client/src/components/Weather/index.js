e4ae1084271e5c489a5761a869dbb842539f9f6e
Anderson <andersonswaimboyd@gmail.com>
2019-08-11 19:10:25 -0500

    styled weather, added media screens

--- client/src/components/Weather/index.js
+++ client/src/components/Weather/index.js
@@ -26,10 +26,13 @@ const Weather = () => {
     let er;
     let max;
     let min;
+    let humid;
+    let icon;
     if(weather.weather){
         console.log(weather.weather[0])
         wea = weather.weather[0].main;
         th = weather.weather[0].description;
+        icon = weather.weather[0].icon;
     }
     console.log(weather.main);
     if(weather.main){
@@ -37,17 +40,20 @@ const Weather = () => {
         er = Math.round(((weather.main.temp - 273.15) * 9 / 5 + 32));
         max = Math.round(((weather.main.temp_max - 273.15) * 9 / 5 + 32));
         min = Math.round(((weather.main.temp_min - 273.15) * 9 / 5 + 32));
+        humid = weather.main.humidity;
     }
     console.log(weather.main);
 
     return (
         <div className="weather">
             <h5>Weather in {weather.name}</h5>
-                   <p>
-                        <span className="info"><strong>{wea}:</strong> {th}, {er}°F<br /></span>
-                        <span className="hi"><strong>Today's High: </strong>{max}°F | </span>
-                        <span className="lo"><strong>Today's Low: </strong>{min}°F</span>
-                    </p>
+            <span className="weather-block">
+                <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="icon" />
+                <span className="info"><strong>{wea}:</strong> {th}, {er}°F</span>
+                <span className="hi"><strong>Today's High: </strong>{max}°F</span>
+                <span className="lo"><strong>Today's Low: </strong>{min}°F</span>
+                <span className="humid"><strong>Humidity: </strong>{humid}%</span>
+            </span>
         </div>
     )
 
